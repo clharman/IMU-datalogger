0fa74c9a047b24ec1f15afc5a738814688842486
Colin Luongo Harman <clharman@umich.edu>
2014-04-23 22:17:47 +0000

    correct 2s comp accel. wrong gyro,mag.

--- LSM303D.py
+++ LSM303D.py
@@ -84,13 +84,13 @@ def m_init(bus, m_FS = m_FS_4g):	#returns conversion factor = gain (output will 
 
 def get_a(bus, a_gain):	#returns [a_x, a_y, a_z] (mg)
 	a_x = 256 * bus.read_byte_data(adr,a_xhi) + bus.read_byte_data(adr,a_xlo)
-	if(a_x >= 37268): a_x = a_x - 74536
+	if(a_x >= 32768): a_x = a_x - 65536
 
 	a_y = 256 * bus.read_byte_data(adr,a_yhi) + bus.read_byte_data(adr,a_ylo)
-	if(a_y >= 37268): a_y = a_y - 74536
+	if(a_y >= 32768): a_y = a_y - 65536
 
-	a_z = 256 * bus.read_byte_data(adr,a_yhi) + bus.read_byte_data(adr,a_ylo)
-	if(a_z >= 37268): a_z = a_z - 74536
+	a_z = 256 * bus.read_byte_data(adr,a_zhi) + bus.read_byte_data(adr,a_zlo)
+	if(a_z >= 32768): a_z = a_z - 65536
 
 	a_x = a_gain * a_x
 	a_y = a_gain * a_y
@@ -100,13 +100,13 @@ def get_a(bus, a_gain):	#returns [a_x, a_y, a_z] (mg)
 
 def get_m(bus, m_gain):	#returns [m_x, m_y, m_z] (mgauss)
 	m_x = 256 * bus.read_byte_data(adr,m_xhi) + bus.read_byte_data(adr,m_xlo)
-	if(m_x >= 37268): m_x = m_x - 74536
+	if(m_x >= 65536): m_x = m_x - 131072
 
 	m_y = 256 * bus.read_byte_data(adr,m_yhi) + bus.read_byte_data(adr,m_ylo)
-	if(m_y >= 37268): m_y = m_y - 74536
+	if(m_y >= 65536): m_y = m_y - 131072
 
 	m_z = 256 * bus.read_byte_data(adr,m_zhi) + bus.read_byte_data(adr,m_zlo)
-	if(m_z >= 37268): m_z = m_z - 74536
+	if(m_z >= 65536): m_z = m_z - 131072
 
 	m_x = m_gain * m_x
 	m_y = m_gain * m_y
--- acceltest.py
+++ acceltest.py
@@ -3,12 +3,12 @@ import LSM303D as a
 import lowpass as lp
 
 bus = a.bus_init()
-gain = a.gyro_init(bus)
+gain = a.a_init(bus)
 last = [0,0,0]
 
 while(1):
 	axl = a.get_a(bus, gain)
 
-	print str(lp.simple(axl[0], last[0])) + '\t' + str(lp.simple(axl[1], last[1])) + '\t' + str(lp.simple(axl[2], last[2]))
+	print str(round(lp.simple(axl[0], last[0])/1000,2)) + '\t' + str(round(lp.simple(axl[1], last[1])/1000,2)) + '\t' + '\t' + str(round(lp.simple(axl[2], last[2])/1000,2))
 	last = axl
 	time.sleep(0.1)
