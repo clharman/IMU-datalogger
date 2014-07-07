b6441323308343be5bcbd6a6ab14299be5f74353
Colin Luongo Harman <clharman@umich.edu>
2014-07-07 00:30:52 -0400

    added references to L3GD20

--- gyrotest.py
+++ gyrotest.py
@@ -1,11 +1,11 @@
 import time
-import L3GD20
+import L3GD20 as g
 
-bus = bus_init()
-gain = gyro_init(bus)
+bus = g.bus_init()
+gain = g.gyro_init(bus)
 
 while(1):
-	rates = get_gyro(bus, gain)
+	rates = g.get_gyro(bus, gain)
 	print rates(1) + '\t' + rates(2) + '\t' + rates(3)
 	time.sleep(0.1)
 
