9a0add790791b3049cfaa9a086e94336c9d5f3d8
Colin Luongo Harman <clharman@umich.edu>
2014-07-07 00:33:59 -0400

    fixed list accessing

--- gyrotest.py
+++ gyrotest.py
@@ -6,6 +6,6 @@ gain = g.gyro_init(bus)
 
 while(1):
 	rates = g.get_gyro(bus, gain)
-	print rates(1) + '\t' + rates(2) + '\t' + rates(3)
+	print rates[1] + '\t' + rates[2] + '\t' + rates[3]
 	time.sleep(0.1)
 
