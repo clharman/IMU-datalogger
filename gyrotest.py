669e303f8cb71ccec65e68e2f1759ab0d2d7785f
Colin Luongo Harman <clharman@umich.edu>
2014-07-07 21:01:03 -0400

    created lp filter, updated gyrotest to implement lp
    lp includes 1 simple filter y)n = x)n + x) n-1

--- gyrotest.py
+++ gyrotest.py
@@ -1,11 +1,15 @@
 import time
 import L3GD20 as g
+import lowpass as lp
 
 bus = g.bus_init()
 gain = g.gyro_init(bus)
+last = 0
 
 while(1):
 	rates = g.get_gyro(bus, gain)
-	print str(rates[0]-4.2) +'\t'+ '\t' + str(rates[1]-1.3) +'\t'+ '\t' + str(rates[2]-1.3)
-	time.sleep(0.1)
+
+	print str(rates[0]) +'\t' + '\t' + lp.simple(rates[0], last)
+	last = rates[0]
+	time.sleep(0.05)
 
---
+++ lowpass.py
@@ -0,0 +1,3 @@
+#simple: y(n) = x(n) + x(n-1)
+def simple(x1, x0 = 0):
+	return x1 + x0
